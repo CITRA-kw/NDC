f9ec38f8ab93d601357ad21a196d3619c142cf00
SalehCoder <saleh_rambo@hotmail.com>
2018-11-12 11:22:58 +0300

    testing purposes

--- app.js
+++ app.js
@@ -72,7 +72,8 @@ app.use(function(err, req, res, next) {
     console.log("** Error");
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error =  err;
 
   // render the error page
   res.status(err.status || 500);
